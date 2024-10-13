7d4637b926f81974ea5bca41be0c740865c03e8d
durga shetty <durgalaxmi.shetty@f1studioz.com>
2024-10-13 23:20:48 +0530

    fixes 4

--- App.js
+++ App.js
@@ -34,8 +34,6 @@ function scrollPlay() {
         vid0.style.opacity = fadeAmount < 0 ? 0 : fadeAmount; // Fade out as scrolling continues
     }
 
-    
-
     window.requestAnimationFrame(scrollPlay);
 }
 
@@ -68,3 +66,27 @@ window.addEventListener('scroll', function() {
 });
 
 
+
+
+
+// var frameNumber = 0, // start video at frame 0
+//     // lower numbers = faster playback
+//     playbackConst = 500, 
+//     // get page height from video duration
+//     setHeight = document.getElementById("set-height"), 
+//     // select video element         
+//     vid = document.getElementById('v0'); 
+
+// // dynamically set the page height according to video length
+// vid.addEventListener('loadedmetadata', function() {
+//   setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+// });
+
+// // Use requestAnimationFrame for smooth playback
+// function scrollPlay(){  
+//   var frameNumber  = window.pageYOffset/playbackConst;
+//   vid.currentTime  = frameNumber;
+//   window.requestAnimationFrame(scrollPlay);
+// }
+
+// window.requestAnimationFrame(scrollPlay);
