3664cb0046e3fa881eeaccd84a51eac0568871a9
durga shetty <durgalaxmi.shetty@f1studioz.com>
2024-10-16 00:06:30 +0530

    fixes 3

--- version3/version3.js
+++ version3/version3.js
@@ -1,7 +1,7 @@
 
 var frameNumber = 0, // start video at frame 0
     // lower numbers = faster playback
-    playbackConst = 980, 
+    playbackConst = 900, 
     // get page height from video duration
     setHeight = document.getElementById("set-height"), 
     // select video element         
